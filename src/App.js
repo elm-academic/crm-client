b82b40a1d67171be0c0c18361aef379b8c65e319
meriemf <farsi.meriem60@gmail.com>
2020-11-28 23:01:55 -0500

    add useState declaration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 //import  Navbar  from './components/Navbar';
